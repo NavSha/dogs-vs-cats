3d318f290685bc236c3833122dfbb900727f8082
Navneet Sharma <NavSha@Richa-MacBook.local>
2019-11-28 16:28:17 -0800

    More refactoring by organizing remaining items as functions

--- dogs_and_cats.py
+++ dogs_and_cats.py
@@ -12,81 +12,25 @@ Returns:
 import os,shutil
 from keras import layers
 from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-'''
-original_dataset_dir = '/Users/NavSha/Documents/tensorflow-projects/Cat_Dog_data'
-original_dataset_dir_cat = '/Users/NavSha/Documents/tensorflow-projects/Cat_Dog_data/Cat/'
-original_dataset_dir_dog = '/Users/NavSha/Documents/tensorflow-projects/Cat_Dog_data/Dog'
-#set base directory
+IMG_WIDTH, IMG_HEIGHT = 150, 150
+
+#set up base, training, validation and test directories
 base_dir = '/Users/NavSha/Documents/tensorflow-projects/cats_and_dogs_small'
-os.mkdir(base_dir)
-
-
-#create training, validation and test directories
 train_dir = os.path.join(base_dir, 'train')
-os.mkdir(train_dir)
 validation_dir = os.path.join(base_dir,'validation')
-os.mkdir(validation_dir)
 test_dir = os.path.join(base_dir,'test')
-os.mkdir(test_dir)
 
-# create dogs and cats directories inside training, validation and test directories
-train_cats_dir = os.path.join(train_dir,'cats')
-os.mkdir(train_cats_dir)
-
-train_dogs_dir = os.path.join(train_dir,'dogs')
-os.mkdir(train_dogs_dir)
-
-validation_cats_dir = os.path.join(validation_dir,'cats')
-os.mkdir(validation_cats_dir)
-
-validation_dogs_dir = os.path.join(validation_dir,'dogs')
-os.mkdir(validation_dogs_dir)
-
-test_cats_dir = os.path.join(test_dir,'cats')
-os.mkdir(test_cats_dir)
-
-test_dogs_dir = os.path.join(test_dir,'dogs')
-os.mkdir(test_dogs_dir)
-
-# copy 1000 cats images to the cats training directory
-fnames = ['{}.jpg'.format(i) for i in range(1000)]
-for fname in fnames:
-    src = os.path.join(original_dataset_dir_cat, fname)
-    dst = os.path.join(train_cats_dir,fname)
-    shutil.copyfile(src,dst)
-# copy 500 cats images to cats validation directory
-fnames = ['{}.jpg'.format(i) for i in range(1000,1500)]
-for fname in fnames:
-    src = os.path.join(original_dataset_dir_cat, fname)
-    dst = os.path.join(validation_cats_dir,fname)
-    shutil.copyfile(src,dst)
-#copy 500 cats images to the cats testing directory
-fnames = ['{}.jpg'.format(i) for i in range(1500,2000)]
-for fname in fnames:
-    src = os.path.join(original_dataset_dir_cat, fname)
-    dst = os.path.join(test_cats_dir,fname)
-    shutil.copyfile(src,dst)
-
-# copy 1000 dogs images to the cats training directory
-fnames = ['{}.jpg'.format(i) for i in range(1000)]
-for fname in fnames:
-    src = os.path.join(original_dataset_dir_dog, fname)
-    dst = os.path.join(train_dogs_dir,fname)
-    shutil.copyfile(src,dst)
-#copy 500 dogs images to the dogs validation directoy
-fnames = ['{}.jpg'.format(i) for i in range(1000,1500)]
-for fname in fnames:
-    src = os.path.join(original_dataset_dir_dog, fname)
-    dst = os.path.join(validation_dogs_dir,fname)
-    shutil.copyfile(src,dst)
-#copy 500 dogs images to the testing directory
-fnames = ['{}.jpg'.format(i) for i in range(1500,2000)]
-for fname in fnames:
-    src = os.path.join(original_dataset_dir_dog, fname)
-    dst = os.path.join(test_dogs_dir,fname)
-    shutil.copyfile(src,dst)
-'''
+def img_channel_type():
+    # Pre-preprocessing of image
+    if K.image_data_format() == 'channels_first':
+        input_shape = (3, IMG_WIDTH, IMG_HEIGHT)
+    else:
+        input_shape = (IMG_WIDTH, IMG_HEIGHT, 3)
 
 def create_model():
 # create a model with 3 conv layers and 3 maxpooling layers
@@ -104,41 +48,47 @@ def create_model():
     model.add(layers.Dense(1,activation = 'sigmoid'))
     return model
 
-#compile the model
-from keras import optimizers
-model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4),metrics=['acc'])
+def training():
+    # data preprocessing using ImageDataGenerator
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1./255)
 
-# data preprocessing using ImageDataGenerator
-from keras.preprocessing.image import ImageDataGenerator
+    train_generator = train_datagen.flow_from_directory(train_dir,target_size=[IMAGE_WIDTH,IMAGE_HEIGHT],batch_size=20,class_mode='binary')
 
-train_datagen = ImageDataGenerator(rescale=1./255)
-test_datagen = ImageDataGenerator(rescale=1./255)
+    validation_generator = test_datagen.flow_from_directory(validation_dir,target_size=[IMAGE_WIDTH,IMAGE_HEIGHT],batch_size=20,class_mode='binary')
 
-train_generator = train_datagen.flow_from_directory(train_dir,target_size=(150,150),batch_size=20,class_mode='binary')
+    model = create_model()
+    #compile the model
+    model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4),metrics=['acc'])
 
-validation_generator = test_datagen.flow_from_directory(validation_dir,target_size=(150,150),batch_size=20,class_mode='binary')
+    history = model.fit_generator(train_generator, steps_per_epoch=100,epochs=10,validation_data = validation_generator,validation_steps=50)
+    model.save('cats_and_dogs_small_1.h5')
 
-history = model.fit_generator(train_generator, steps_per_epoch=100,epochs=10,validation_data = validation_generator,validation_steps=50)
-model.save('cats_and_dogs_small_1.h5')
+    model_json = model.to_json()
+    with open('model_ugc.json', "w") as json_file:
+        json_file.write(model_json)
 
+def plot_loss_and_accuracy():
 #let's plot the training and validation losses and accuracies
-import matplotlib.pyplot as plt
+    acc = history.history['acc']
+    loss = history.history['loss']
+    val_acc = history.history['val_acc']
+    val_loss = history.history['val_loss']
 
-acc = history.history['acc']
-loss = history.history['loss']
-val_acc = history.history['val_acc']
-val_loss = history.history['val_loss']
+    epochs = range(1,len(acc)+1)
 
-epochs = range(1,len(acc)+1)
+    plt.plot(epochs, acc, 'bo', label = 'Training acc')
+    plt.plot(epochs,val_acc,'bo',label = 'Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.figure()
 
-plt.plot(epochs, acc, 'bo', label = 'Training acc')
-plt.plot(epochs,val_acc,'bo',label = 'Validation acc')
-plt.title('Training and validation accuracy')
-plt.legend()
-plt.figure()
+    plt.plot(epochs, loss, 'ro', label = 'Training loss')
+    plt.plot(epochs,val_acc,'ro',label = 'Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
 
-plt.plot(epochs, loss, 'ro', label = 'Training loss')
-plt.plot(epochs,val_acc,'ro',label = 'Validation loss')
-plt.title('Training and validation loss')
-plt.legend()
-plt.show()
+if __name__=="__main__":
+    img_channel_type()
+    training()
